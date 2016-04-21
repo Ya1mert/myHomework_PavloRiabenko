1682ec5038e7e25f30ef6cb083071014eedbc589
ya1mert <yamert@live.com>
2016-04-21 20:50:43 +0300

    changed gitignore

--- lesson#1/test.js
+++
@@ -1,9 +0,0 @@
-colors = ['#red','#cyan','#teal','#green','#blue','#lime','#yellow','#silver'];
-
-
-
-var x = 'flibust';
-for (i=0 ; i< x.length ; i++){
-    var result='';
-    result+=colors[i]+'{'+x.charAt(i)+'}';
-    } console.log(result);
